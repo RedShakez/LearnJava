8b4fc46137ba0daa74db033d00664e10887ba44c
RedShakez <shakez808@outlook.com>
2022-06-30 23:17:09 -0400

    Ran Main did a little testing with it as well and also learned if else elseif statements

---
+++ If_Statements/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 18;
+        
+        if(age >= 18)
+            System.out.println("You have a high school degree... hopefully");
+        else 
+            System.out.println("Don't watch Porn");
+        
+    }
+}
