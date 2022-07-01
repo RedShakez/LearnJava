0b7094d4ec16b3644c4ccc78f6119dd49c7a32d6
RedShakez <shakez808@outlook.com>
2022-07-01 11:37:44 -0400

    Made a program to count down infinitly well until -2 billion

---
+++ Loops_and_Statements/For_Loop.java
@@ -0,0 +1,10 @@
+//run if you want to see numbers go so low 
+public class For_Loop {
+   
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 10; i--) { //index is i 
+            System.out.println(i);
+        }
+    } 
+}
